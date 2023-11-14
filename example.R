d202a90c8c12fceeec3b9def23984a79a3795df1
Savva Konstantinov <savvakonst@gmail.com>
2023-11-14 15:19:52 +0300

    Create example.R

---
+++ example.R
@@ -0,0 +1,23 @@
+message <-"Hello World!"
+print(message)  
+
+message <-"Hello World!"
+print(message)  
+
+
+
+# this is a link to the first chapter
+#https://github.com/savvakonst/temp_docs/tree/main#%D0%B1%D1%80%D0%BE%D1%81%D0%B0%D0%BD%D0%B8%D0%B5-%D0%BC%D0%BE%D0%BD%D0%B5%D1%82%D1%8B
+
+message <-"Hello World!"
+print(message)  
+
+# this is a link to the second chapter
+https://github.com/savvakonst/temp_docs/tree/main#%D0%B5%D1%81%D0%BB%D0%B8-%D0%B8%D0%BD%D1%82%D0%B5%D1%80%D0%B5%D1%81%D0%BD%D0%BE-%D0%BF%D0%BE%D1%87%D0%B5%D0%BC%D1%83-pxqx--pxq-%D1%81%D0%BC%D0%BE%D1%82%D1%80%D0%B8-%D0%B4%D0%B0%D0%BB%D0%B5%D0%B5
+message <-"Hello World!"
+print(message)  
+
+
+
+message <-"Hello World!"
+print(message)  
